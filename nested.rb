1dbce401908c3e56038db293fbbc74389a1a76d9
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-01 15:47:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -95,7 +95,7 @@ def changing_alan
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"],
-          alans_new_info => "GUI"
+          alans_new_info = "GUI"
         },
         :dennis_ritchie => {
           :known_for => "Unix",
