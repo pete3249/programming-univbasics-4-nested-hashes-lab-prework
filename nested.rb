16fb51779c02006a1b73128f0d4cca9f37e460be
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-01 16:03:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,7 @@ def changing_alan
         }
  		}
   alans_new_info = "GUI"
-  programmer_hash[:alan_kay][alans_new_info]
+  programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
 end
 
