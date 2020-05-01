467de224a9dcff7ac07a70c24e1f4d67483a802d
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-01 16:00:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,6 @@ programmer_hash
 end
 
 def changing_alan
-  alans_new_info = "GUI"
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -103,7 +102,7 @@ def changing_alan
         }
  		}
   alans_new_info = "GUI"
-  programmer_hash[:alan_kay][:known_for] = alans_new_info.to_s
+  programmer_hash[:alan_kay][:known_for] = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
 end
 
