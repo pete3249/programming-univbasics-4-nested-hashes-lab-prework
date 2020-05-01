70ac9d042a45ad49a5e63bcfb366d65d7a19743c
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-01 15:50:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay][:known_for] = GUI
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      #alans_new_info = "GUI"
